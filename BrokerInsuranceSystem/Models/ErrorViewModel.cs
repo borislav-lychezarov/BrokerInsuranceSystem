4743313310b541626aef21bb5b13821f9c12a9cb
Borislav <borislavlychezarov@gmail.com>
2020-11-16 21:25:00 +0200

    Add project files.

---
+++ BrokerInsuranceSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrokerInsuranceSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
